0865530bd7bf26d207ab5e9ab232ea718c5332e4
Amartya Aishwarya <amartya970914@gmail.com>
2018-12-23 17:34:31 +0530

    vd 2show page 2

--- v2/app.js
+++ v2/app.js
@@ -50,7 +50,8 @@ app.post("/campgrounds", function(req, res){
     //res.send("post route");
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc};
     //create new campground and save 
     Campground.create(newCampground, function(err, newlyCreated){
             if(err){
